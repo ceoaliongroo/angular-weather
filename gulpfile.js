0406c01b3fbc14e8dde6646bce05dbb8268bd7c8
Carlos Mantilla <ceo@aliongroo.com>
2015-07-02 00:13:26 +0300

    chore: add build task.

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,16 @@ var clean = require('gulp-clean');
 var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
 
-gulp.task('default', function() {
-  return gulp.src('*.js')
-    .pipe(clean())
+gulp.task('clean:dist', function () {
+  return gulp.src('dist')
+    .pipe(clean());
+});
+
+gulp.task('build', ['clean:dist'], function() {
+  return gulp.src('src/**/*.js')
     .pipe(ngAnnotate())
     .pipe(uglify())
     .pipe(gulp.dest('dist'));
 });
+
+gulp.task('default', ['build']);
