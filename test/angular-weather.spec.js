b9b46eee6de17b331a9de78179a56fbd0b52c57e
Carlos Mantilla <ceo@aliongroo.com>
2015-07-21 00:03:14 +0300

    feat(weather): improve the cache mechanism, test(all): define basic workflow for test.

---
+++ test/angular-weather.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('angular-weather', function() {
+  describe('dummy test', function() {
+
+    it('should ', function() {
+      expect(true).toEqual(true);
+    });
+  });
+});
